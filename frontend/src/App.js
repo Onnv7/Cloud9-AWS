5e61d2123ab0682b88201018ae2a149231ac87b2
KhoaTT <khoanono963@gmail.com>
2022-11-28 10:06:52 +0700

    update khoa 28/11 #2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,14 +17,11 @@ import "./style/dark.scss";
 
 import { useContext } from "react";
 
-import { DarkModeContext } from "./context/darkModeContext";
 
 
 function App() {
-  const { darkMode } = useContext(DarkModeContext);
 
   return (
-    <div className={darkMode ? "app dark" : "app"}>
 
       <BrowserRouter>
         <div className="App">
@@ -53,7 +50,6 @@ function App() {
           <Footer />
         </div>
       </BrowserRouter>
-    </div>
   );
 }
 
