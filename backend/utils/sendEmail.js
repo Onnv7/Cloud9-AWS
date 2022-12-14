0911911369912360bc9bc885e3f4eb24b80a5894
Nguyễn Văn An <nva6112002@gmail.com>
2022-12-14 10:39:39 +0700

    config email smtp

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -10,8 +10,8 @@ export const sendEmail = async (email, subject, text) => {
 			port: Number(process.env.EMAIL_PORT),
 			secure: Boolean(process.env.SECURE),
 			auth: {
-				user: process.env.USER,
-				pass: process.env.PASS,
+				user: "nva6112002@gmail.com",
+				pass: "aopkjaqlnmabvzia",
 			},
 		});
 		await transporter.sendMail({
