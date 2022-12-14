9c8e69811b200e3fc44f870fdf2242008143e0c7
Nguyễn Văn An <nva6112002@gmail.com>
2022-12-14 10:19:28 +0700

    ok

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,12 +6,41 @@ import { saveFileObj } from "../utils/saveFile.js";
 import { getUrlImageObj } from "../utils/getUrlImage.js";
 import { generateCode } from "../utils/generatePassword.js";
 import { sendEmail } from "../utils/sendEmail.js";
+import { v4 as uuidv4 } from 'uuid';
+import AWS from "aws-sdk"
+let awsConfig = {
+  "region": "us-east-1"
+}
 
+AWS.config.update(awsConfig)
+
+let docClient = new AWS.DynamoDB.DocumentClient();
 export const sendCodeVerify = async (req, res, next) => {
   try {
-    const code = String(await generateCode(6));
-    await sendEmail(req.body.email, "Your code", code);
-    res.status(200).json({ code });
+    const id = uuidv4();
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const address = "%Phường Phúc Xá%Quận Ba Đình%Thành Phố Hà Nội"
+    const image = req.body.img;
+    const input = {
+      "address": address,
+      ...req.body,
+      "password": hash,
+      "id": id,
+      "imgPath": "/assets/images/default-user.png"
+    }
+    var params = {
+      TableName: "users",
+      Item: input
+    }
+    docClient.put(params, function (err, data) {
+      if (err) {
+        console.log("ERROR", JSON.stringify(err, null, 2));
+      }
+      else {
+        res.status(200).json("Created")
+      }
+    })
   } catch (error) {
     next(error);
   }
@@ -40,40 +69,61 @@ export const register = async (req, res, next) => {
 // login to set token
 export const login = async (req, res, next) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) return next(createError(404, "User not found!"));
+    var user;
+    var params = {
+      TableName: "users",
+      FilterExpression: "#email= :email",
 
-    // compare password in mongoose vs frontend
-    const isPasswordCorrect = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    if (!isPasswordCorrect) {
-      res.status(200).json({
-        status: "Wrong password or email!",
-        success: false
-      });
-      return;
+      ExpressionAttributeNames: {
+        "#email": "email"
+      },
+      ExpressionAttributeValues: {
+        ":email": req.body.email
+      },
+
     }
-    //return next(createError(400, "Wrong password or email!"));
+    await docClient.scan(params, function (err, data) {
+      if (err) {
+        console.log("ERROR", JSON.stringify(err, null, 2));
+      }
+      else {
+        user = data.Items[0]
+        if (!user) return next(createError(404, "User not found!"));
 
-    // create a new token for backend
-    const token = jwt.sign(
-      { id: user._id, isAdmin: user.isAdmin },
-      process.env.JWT
-    );
-    const { password, img, ...otherDetails } = user._doc;
-    // set cookie token for backend
-    let imgPath;
-    if (user.img === null)
-      imgPath = user.avatar;
-    else
-      imgPath = getUrlImageObj(img);
-    //res.cookie("access_token")
-    res
-      .cookie("access_token", token)
-      .status(200)
-      .json({ ...otherDetails, imgPath: imgPath });
+
+        // compare password in mongoose vs frontend
+        const isPasswordCorrect = bcrypt.compare(
+          req.body.password,
+          user.password
+        );
+        if (!isPasswordCorrect) {
+          res.status(200).json({
+            status: "Wrong password or email!",
+            success: false
+          });
+          return;
+        }
+        //return next(createError(400, "Wrong password or email!"));
+
+        // create a new token for backend
+        const token = jwt.sign(
+          { id: user._id, isAdmin: user.isAdmin },
+          process.env.JWT
+        );
+        const { password, img, ...otherDetails } = user;
+        // set cookie token for backend
+        let imgPath;
+        if (user.img === undefined)
+          imgPath = user.avatar;
+        else
+          imgPath = getUrlImageObj(img);
+        //res.cookie("access_token")
+        res
+          .cookie("access_token", token)
+          .status(200)
+          .json({ ...otherDetails, imgPath: imgPath });
+      }
+    })
   } catch (err) {
     next(err);
   }
